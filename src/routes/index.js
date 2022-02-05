9eff5a2ca441edef11c4cf03fdf63fb2eac8c45e
Jonathan Manchego Sosa <jonathan571999@gmail.com>
2022-02-05 12:24:36 -0500

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,9 @@ const clientes = require('./clientes')
 
 
 module.exports = app => {
-  app.get('/', (req,res) => res.send('hola'))
-  app.use('/', clientes)
+  app.get('/', (req,res) => res.json({error: false, msg: 'hola'}));
+  app.use('/', clientes);
   app.get('***', (req, res) => {
     res.send('Fin')
-  })
+  });
 }
